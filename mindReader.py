f9d8f7304437c7464656d3d86a7fc79b424ed311
Arnav-panjla <arnavpanjla@gmail.com>
2024-01-06 08:02:00 +0530

    funct to check input added

--- mindReader.py
+++ mindReader.py
@@ -1,9 +1,18 @@
 from tkinter import *
 from tkinter.ttk import *
+from tkinter import messagebox 
 import time
 
 running = True
 Text = ("Analyzing brainwaves...", "Scanning memories...", "Calculating probabilities...", "Decoding thoughts...")
+validInput = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+
+def checkInput():
+    if entrybox.get() in validInput:
+        click()
+    else:
+        messagebox.showerror(title="Invalid Input", message="Please enter an valid input")
 
 
 def  finalResult():
@@ -20,7 +29,7 @@ def update_progressbar(value):
     bar['value'] = value
     textDisplay(value)
     if value <= 100:
-        window1.after(50, update_progressbar, value + 1) # ##############################################
+        window1.after(50, update_progressbar, value + 1) # widget.after(delay{in ms}, callback function, *args) 
     else:
         time.sleep(0.1)
         window1.destroy()
@@ -31,12 +40,14 @@ def click():
     global window1
     window1 = Tk()
     window1.geometry("300x100")
-    global bar, label
+    window1.title("Reading your mind")
+    global bar
     bar = Progressbar(window1, orient=HORIZONTAL, length=250)
     bar.place(x=25, y=50)
+    global label
     label = Label(window1, text="")
     label.pack()
-    window1.after(50, update_progressbar, 0)
+    window1.after(50, update_progressbar, 0)# widget.after(delay{in ms}, callback function, *args)
 
 window = Tk()
 
@@ -45,7 +56,7 @@ Label(window, text="Think of a number between 1 and 10:").place(x=50, y=30)
 entrybox = Entry(window, font=("consol", 10))
 entrybox.place(x=80, y=60)
 
-Button(window, text="Read my mind", command=click).place(x=100, y=90)
+Button(window, text="Read my mind", command=checkInput).place(x=100, y=90)
 
 window.title("Mind Reader")
 window.geometry("300x150")
